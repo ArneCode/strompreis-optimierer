d25d83da3d6aaff9b5654256d2b2884f7da39249
Julian Meilhammer <Julian@meilhammer.de>
2025-12-15 14:33:48 +0100

    s

--- my-app/src/pages/Home.jsx
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import './Header.css'; // relativer Pfad zur CSS-Datei
-
-export default function Home() {
-    return <h1 className="header-box">Startseite</h1>;
-}
--- my-app/src/pages/Household.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-import './Header.css'; // relativer Pfad zur CSS-Datei
-
-export default function Household() {
-    return <h1 className="header-box">Mein Haushalt</h1>;
-}
