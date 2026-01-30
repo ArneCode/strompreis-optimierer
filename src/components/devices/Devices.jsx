dcfdd2a338e674402e8327a8e5a9a46bb7f1a48e
Jannis242 <uhiyh@student.kit.edu>
2026-01-30 08:03:20 +0100

    added different error messages

--- src/components/devices/Devices.jsx
+++ src/components/devices/Devices.jsx
@@ -1,4 +1,4 @@
-import {useEffect, useState} from 'react';
+import {useState} from 'react';
 import '../../styles/components/Devices.css';
 import Device from './Device.jsx';
 import DeviceForm from './DeviceForm';
