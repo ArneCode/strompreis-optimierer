09cac708e9fa70c48a6d8e52d74f5f4925d992b0
Julian Meilhammer <Julian@meilhammer.de>
2026-02-10 01:12:14 +0100

    geoservice updated

--- src/services/geoService.jsx
+++ src/services/geoService.jsx
@@ -1,15 +1,13 @@
 export const geoService = {
     async getAddressFromCoords(lat, lng) {
-        try {
-            const response = await fetch(
-                `https://nominatim.openstreetmap.org/reverse?format=jsonv2&lat=${lat}&lon=${lng}`
-            );
-            if (!response.ok) throw new Error('Netzwerk-Fehler');
+        const response = await fetch(
+            `https://nominatim.openstreetmap.org/reverse?format=jsonv2&lat=${lat}&lon=${lng}`
+        );
 
-            const data = await response.json();
-            return data;
-        } catch (error) {
-            throw error;
+        if (!response.ok) {
+            throw new Error(`API_ERROR: ${response.status}`);
         }
+
+        return await response.json();
     }
 };
