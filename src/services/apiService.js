bdf8682db1acc380711218d3f0dd8c8a15d8b571
uklfk <uklfk@student.kit.edu>
2026-03-06 12:56:14 +0100

    baseURL geändert

--- src/services/apiService.js
+++ src/services/apiService.js
@@ -6,7 +6,7 @@ import { mapDeviceData } from './deviceMapper';
  */
 class ApiService {
     constructor() {
-        this.baseURL = "http://127.0.0.1:5000/api";
+        this.baseURL = "/api";
     }
 
     /**
